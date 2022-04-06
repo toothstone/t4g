4287aca50dfcdef0771b6b6c1e86a6ff1aa13b63
toothstone <f.zahn@mailbox.org>
2022-04-06 21:28:27 +0200

    Start basic table display

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { MatTableDataSource } from '@angular/material/table';
+
+export interface Datum {
+  department: string;
+  description: string;
+  datasets: number;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  constructor(
+    private http: HttpClient
+  ) {}
+
+  dataSource!: MatTableDataSource<Datum>;
+
   title = 'datasets-dashboard';
+  columnsToDisplay = ["department", "description", "datasets"]
+  
+  ngOnInit() {
+    this.getDataSets()
+      .subscribe(response => {
+        this.dataSource = new MatTableDataSource(response);
+      });
+  }
+  getDataSets() {
+    return this.http.get<Datum[]>('https://raw.githubusercontent.com/tech4germany/coding-challenge/main/backend-response.json');
+  }
+
+  applyFilter(filterValue: string) {
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatTableModule } from '@angular/material/table';
+import { MatSortModule } from '@angular/material/sort'
+import { MatFormFieldModule } from '@angular/material/form-field'
+
 
 @NgModule({
   declarations: [
@@ -10,7 +17,12 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    AppRoutingModule,
+    MatTableModule,
+    MatSortModule,
+    MatFormFieldModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
